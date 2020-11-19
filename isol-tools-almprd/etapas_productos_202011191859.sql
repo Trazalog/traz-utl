f2fbed605f418896117070e6b532d7028ec77487
Rodolfo <rruiz@trazalog.com>
2020-11-19 19:01:41 -0300

    articulos isola

---
+++ isol-tools-almprd/etapas_productos_202011191859.sql
@@ -0,0 +1,30 @@
+INSERT INTO prd.etapas_productos (etap_id,arti_id) VALUES 
+(1,1285)
+,(1,1286)
+,(1,1287)
+,(1,1288)
+,(1,1289)
+,(1,1290)
+,(1,1291)
+,(1,1292)
+,(1,1293)
+,(1,1294)
+;
+INSERT INTO prd.etapas_productos (etap_id,arti_id) VALUES 
+(1,1295)
+,(1,1296)
+,(1,1297)
+,(1,1298)
+,(1,1299)
+,(1,1300)
+,(1,1301)
+,(1,1302)
+,(1,1303)
+,(1,1304)
+;
+INSERT INTO prd.etapas_productos (etap_id,arti_id) VALUES 
+(1,1305)
+,(1,1306)
+,(1,1307)
+,(1,1308)
+;
