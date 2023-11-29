0dbd9b59f999fef8c66449966dbac9d9da55199e
Pablo Marrelli <pmarrelli@trazalog.com>
2023-11-29 14:25:21 -0300

    cambio de base script

--- bulkload/asset/cargaeq.sh
+++ bulkload/asset/cargaeq.sh
@@ -1,5 +1,5 @@
 #primero copiamos el archivo de entrada con nombre fijo
 echo Cargando archivo $1 a Equipos
 cp $1 /tmp/eq.dmp
-mysql -u root -p assetv2 -Ns < loadeq.sql > cargaeq.log
+mysql -u root -p asp2tecn -Ns < loadeq.sql > cargaeq.log
 cat cargaeq.log
