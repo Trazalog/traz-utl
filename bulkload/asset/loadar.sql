30abc0eb901eb9cc56ff9412aa65b3a47298aad1
Pablo Marrelli <pmarrelli@trazalog.com>
2024-08-13 12:14:17 -0300

    TICA - carga masiva articulos man

--- bulkload/asset/loadar.sql
+++ bulkload/asset/loadar.sql
@@ -1,2 +1,2 @@
 load data local infile "/tmp/ars.dmp" into table sta_articulos  character set "utf8" fields terminated by "," lines terminated by "\n" ignore 1 lines;
-call bulkload_articulos(6);
+call bulkload_articulos(15);
